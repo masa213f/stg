c5228cb0e426eb4c73baef243e1f53f1459af738
Masayuki Ishii <masa213f@gmail.com>
2022-08-11 17:05:38 +0000

    Rename directory; sound -> audio
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- resource/sound.go
+++ resource/sound.go
@@ -12,9 +12,9 @@ const (
 
 // Raw data fo background music.
 var (
-	//go:embed files/sound/bgm_maoudamashii_fantasy13.mp3
+	//go:embed files/audio/bgm_maoudamashii_fantasy13.mp3
 	RawDataBGMMenu []byte
-	//go:embed files/sound/bgm_maoudamashii_fantasy15.mp3
+	//go:embed files/audio/bgm_maoudamashii_fantasy15.mp3
 	RawDataBGMPlay []byte
 )
 
@@ -27,12 +27,12 @@ const (
 
 // Raw data of sound effects.
 var (
-	//go:embed files/sound/hitting1.mp3
+	//go:embed files/audio/hitting1.mp3
 	RawDataSEShot []byte
-	//go:embed files/sound/warp1.mp3
+	//go:embed files/audio/warp1.mp3
 	RawDataSEBomb []byte
-	//go:embed files/sound/damage6.mp3
+	//go:embed files/audio/damage6.mp3
 	RawDataSEHit []byte
-	//go:embed files/sound/short_bomb.mp3
+	//go:embed files/audio/short_bomb.mp3
 	RawDataSEDamage []byte
 )
